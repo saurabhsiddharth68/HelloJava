b5f4c7dd429d74724d371a6d5b9609d490633ccf
Saurabh Siddharth <saurabhsiddharth68>
2021-10-19 11:39:45 +0530

    Modified:Welcome Message

--- src/main/java/com/blz/hellojava/HelloJava.java
+++ src/main/java/com/blz/hellojava/HelloJava.java
@@ -3,5 +3,6 @@ package com.blz.hellojava;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello java");
+        System.out.println("Welcome to Hello Java Program");
     }
 }
